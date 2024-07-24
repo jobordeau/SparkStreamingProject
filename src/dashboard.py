d02c67d00f434944f242340a53dbb2642ff1e889
jobordeau <jobordeau@etu.uca.fr>
2024-07-24 04:06:10 +0200

    update

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,6 +1,16 @@
 import streamlit as st
 from pyspark.sql import SparkSession
+import pandas as pd
 import time
+
+# Désactiver l'animation de rafraîchissement
+st.markdown("""
+    <style>
+        .st-ck {
+            animation: none !important;
+        }
+    </style>
+""", unsafe_allow_html=True)
 
 # Créer une session Spark avec les dépendances Delta Lake
 spark = SparkSession.builder \
@@ -12,6 +22,14 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 delta_table_path = "hdfs://localhost:19000/user/sparkStreaming/output"
+monthly_count_path = "hdfs://localhost:19000/user/sparkStreaming/monthlyCount"
+global_stats_path = "hdfs://localhost:19000/user/sparkStreaming/globalStats"
+user_stats_path = "hdfs://localhost:19000/user/sparkStreaming/userStats"
+member_csv_path = "main/resources/members.csv"
+
+# Charger le fichier member.csv
+members_df = spark.read.option("header", "true").option("sep", ";").csv(member_csv_path)
+members_df.createOrReplaceTempView("members_view")
 
 # Ajouter un bouton pour arrêter l'actualisation automatique
 refresh = st.sidebar.checkbox("Auto-refresh", value=True)
@@ -20,28 +38,85 @@ refresh = st.sidebar.checkbox("Auto-refresh", value=True)
 refresh_interval = st.sidebar.slider("Refresh interval (seconds)", min_value=1, max_value=60, value=5)
 
 # Fonction pour charger et afficher les données
-def load_and_display_data():
+def load_and_display_data(selected_year=None):
     # Lire les données de Delta Lake
     df = spark.read.format("delta").load(delta_table_path)
+    monthly_df = spark.read.format("delta").load(monthly_count_path)
+    global_stats_df = spark.read.format("delta").load(global_stats_path)
+    user_stats_df = spark.read.format("delta").load(user_stats_path)
 
-    # Créer une vue temporaire
+    # Créer des vues temporaires
     df.createOrReplaceTempView("delta_real_time_view")
+    monthly_df.createOrReplaceTempView("monthly_count_view")
+    global_stats_df.createOrReplaceTempView("global_stats_view")
+    user_stats_df.createOrReplaceTempView("user_stats_view")
 
     # Requête SQL pour obtenir les données
     word_counts = spark.sql("SELECT word, count FROM delta_real_time_view ORDER BY count DESC LIMIT 10")
+    monthly_counts = spark.sql("""
+        SELECT
+            split(Month, '-')[0] as Year,
+            split(Month, '-')[1] as Month,
+            count
+        FROM monthly_count_view
+        ORDER BY Year, Month ASC
+    """)
+    user_stats = spark.sql("""
+        SELECT 
+            COALESCE(Nickname, `Member Name`, UserID) as User, 
+            totalMessages 
+        FROM user_stats_view 
+        LEFT JOIN members_view 
+        ON user_stats_view.UserID = members_view.`Member ID`
+        ORDER BY totalMessages DESC LIMIT 3
+    """)
+
+    global_stats = spark.sql("""
+        SELECT
+            CASE
+                WHEN ServerID = '761246332215623691' THEN '1i3 2020-2021'
+                ELSE '4IABD'
+            END as ServerName,
+            totalMessages
+        FROM global_stats_view
+    """)
+
+    # Convertir les DataFrames Spark en Pandas DataFrames
+    word_counts_pd = word_counts.toPandas()
+    monthly_counts_pd = monthly_counts.toPandas()
+    global_stats_pd = global_stats.toPandas()
+    user_stats_pd = user_stats.toPandas()
+
+    # Générer un DataFrame avec tous les mois de 1 à 12 pour chaque année
+    all_months = pd.DataFrame({
+        'Month': [str(i).zfill(2) for i in range(1, 13)]
+    })
 
     # Afficher les données avec Streamlit
-    st.title("Word Count Dashboard")
-    st.write("Voici le nombre de mots mis à jour en temps réel:")
+    st.title("Discord Dashboard")
 
-    # Convertir le DataFrame Spark en Pandas DataFrame
-    word_counts_pd = word_counts.toPandas()
+    # Afficher les statistiques globales
+    st.write("Statistiques globales")
+    st.dataframe(global_stats_pd)
 
     # Afficher le DataFrame sous forme de tableau
+    st.write("TOP 10 des mots les plus utilisés")
     st.dataframe(word_counts_pd)
 
-    # Afficher le DataFrame sous forme de graphique
-    st.bar_chart(word_counts_pd.set_index("word"))
+    # Afficher le top 3 des utilisateurs
+    st.write("TOP 3 des utilisateurs ayant envoyé le plus de messages")
+    st.dataframe(user_stats_pd)
+
+    # Sélectionner une année
+    years = monthly_counts_pd['Year'].unique()
+    selected_year = st.sidebar.selectbox("Sélectionnez l'année", options=years, index=len(years) - 1)
+
+    if selected_year:
+        yearly_data = monthly_counts_pd[monthly_counts_pd['Year'] == selected_year]
+        yearly_data = yearly_data.merge(all_months, on='Month', how='right').fillna({'count': 0})
+        yearly_data = yearly_data.sort_values(by='Month')
+        st.write(f"Nombre de messages par mois pour l'année {selected_year}")
+        st.line_chart(yearly_data.set_index('Month')['count'])
 
 # Boucle pour rafraîchir automatiquement les données
 while refresh:
--- src/main/scala/Consumer.scala
+++ src/main/scala/Consumer.scala
@@ -7,12 +7,15 @@ object Consumer {
   def main(args: Array[String]): Unit = {
     val checkpointDir = "hdfs://localhost:19000/user/sparkStreaming/checkpoint"
     val resultPath = "hdfs://localhost:19000/user/sparkStreaming/output"
+    val monthlyCountPath = "hdfs://localhost:19000/user/sparkStreaming/monthlyCount"
+    val globalStatsPath = "hdfs://localhost:19000/user/sparkStreaming/globalStats"
+    val userStatsPath = "hdfs://localhost:19000/user/sparkStreaming/userStats"
 
     val spark = SparkSession.builder
       .appName("StructuredKafkaWordCount")
-      .master("local[4]")
+      .master("local[*]")
       .config("spark.streaming.stopGracefullyOnShutdown", "true")
-      .config("spark.sql.shuffle.partitions", 3)
+      .config("spark.sql.shuffle.partitions", 200)
       .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
       .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
       .getOrCreate()
@@ -43,25 +46,49 @@ object Consumer {
     val messages = df.selectExpr("CAST(value AS STRING)").as[String]
     val messageDF = messages.select(from_json(col("value"), messageSchema).as("data")).select("data.*")
 
+
+    val stopWords = spark.read.textFile("src/main/resources/stopwords-fr.txt").collect().toSet
+
     // Extract and count words in messages
     val words = messageDF
-      .select($"Date", explode(split(col("Message"), "\\s+")).as("word"))
-      .filter(col("word") =!= "")  // Filter out empty words
+      .select($"Date", explode(split(regexp_replace(col("Message"), "'", " "), "\\s+")).as("word"))
+      .withColumn("word", regexp_replace($"word", "[^\\w]+", ""))
+      .withColumn("word", lower($"word"))
+      .filter($"word" =!= "" && !$"word".isin(stopWords.toSeq: _*)) // Filtrer les mots vides et les stop words
       .groupBy("word")
       .count()
 
+    // Count messages by month
+    val monthlyCounts = messageDF
+      .withColumn("Month", date_format(col("Date"), "yyyy-MM"))
+      .groupBy("Month")
+      .count()
+
+    // Count total messages and attachments
+    val globalStats = messageDF
+      .select("ServerID", "Message", "Attachments")
+      .distinct() // Filtrer les doublons potentiels
+      .groupBy("ServerID")
+      .agg(
+        count("Message").as("totalMessages"),
+        count(expr("CASE WHEN Attachments IS NOT NULL AND Attachments != '' THEN 1 END")).as("totalAttachments")
+      )
+
+    // Count messages by user
+    val userStats = messageDF
+      .groupBy("UserID")
+      .agg(
+        count("Message").as("totalMessages")
+      )
+
     // Use foreachBatch for incremental updates
-    val query = words.writeStream
+    val wordCountQuery = words.writeStream
       .outputMode("update")
       .foreachBatch { (batchDF: DataFrame, batchId: Long) =>
-        // Check if the Delta table exists
         if (!DeltaTable.isDeltaTable(spark, resultPath)) {
           batchDF.write.format("delta").mode("overwrite").save(resultPath)
         } else {
-          // Load the existing Delta table
           val deltaTable = DeltaTable.forPath(spark, resultPath)
-
-          // Perform a merge for incremental updates
           deltaTable.as("t")
             .merge(
               batchDF.as("s"),
@@ -77,6 +104,96 @@ object Consumer {
       .option("checkpointLocation", checkpointDir)
       .start()
 
-    query.awaitTermination()
+    val monthlyCountQuery = monthlyCounts.writeStream
+      .outputMode("update")
+      .foreachBatch { (batchDF: DataFrame, batchId: Long) =>
+        if (!DeltaTable.isDeltaTable(spark, monthlyCountPath)) {
+          batchDF.write.format("delta").mode("overwrite").save(monthlyCountPath)
+        } else {
+          val deltaTable = DeltaTable.forPath(spark, monthlyCountPath)
+          deltaTable.as("t")
+            .merge(
+              batchDF.as("s"),
+              "t.Month = s.Month"
+            )
+            .whenMatched
+            .updateAll()
+            .whenNotMatched
+            .insertAll()
+            .execute()
+        }
+      }
+      .option("checkpointLocation", checkpointDir + "_monthly")
+      .start()
+
+    val globalStatsQuery = globalStats.writeStream
+      .outputMode("update")
+      .foreachBatch { (batchDF: DataFrame, batchId: Long) =>
+        if (!DeltaTable.isDeltaTable(spark, globalStatsPath)) {
+          batchDF.write.format("delta").mode("overwrite").save(globalStatsPath)
+        } else {
+          val deltaTable = DeltaTable.forPath(spark, globalStatsPath)
+
+          deltaTable.alias("existing")
+            .merge(
+              batchDF.alias("updates"),
+              "existing.ServerID = updates.ServerID"
+            )
+            .whenMatched
+            .updateExpr(
+              Map(
+                "totalMessages" -> "existing.totalMessages + updates.totalMessages",
+                "totalAttachments" -> "existing.totalAttachments + updates.totalAttachments"
+              )
+            )
+            .whenNotMatched
+            .insertExpr(
+              Map(
+                "ServerID" -> "updates.ServerID",
+                "totalMessages" -> "updates.totalMessages",
+                "totalAttachments" -> "updates.totalAttachments"
+              )
+            )
+            .execute()
+        }
+      }
+      .start()
+
+    val userStatsQuery = userStats.writeStream
+      .outputMode("update")
+      .foreachBatch { (batchDF: DataFrame, batchId: Long) =>
+        if (!DeltaTable.isDeltaTable(spark, userStatsPath)) {
+          batchDF.write.format("delta").mode("overwrite").save(userStatsPath)
+        } else {
+          val deltaTable = DeltaTable.forPath(spark, userStatsPath)
+          deltaTable.as("existing")
+            .merge(
+              batchDF.as("updates"),
+              "existing.UserID = updates.UserID"
+            )
+            .whenMatched
+            .updateExpr(
+              Map(
+                "totalMessages" -> "existing.totalMessages + updates.totalMessages"
+              )
+            )
+            .whenNotMatched
+            .insertExpr(
+              Map(
+                "UserID" -> "updates.UserID",
+                "totalMessages" -> "updates.totalMessages",
+              )
+            )
+            .execute()
+        }
+      }
+      .option("checkpointLocation", checkpointDir + "_user")
+      .start()
+
+
+    wordCountQuery.awaitTermination()
+    monthlyCountQuery.awaitTermination()
+    globalStatsQuery.awaitTermination()
+    userStatsQuery.awaitTermination()
   }
 }
